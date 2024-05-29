e7bde005c8ff1b2b54940cfaa3bc15e094d35e37
Surya <surya.nata.aardhana@gmail.com>
2024-05-29 17:53:55 +0700

    add template

---
+++ resources/views/components/copyright.blade.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="clearfix mb-0 footer text-muted">
+        <div class="float-start">
+            <p>2024 &copy; Surya Nata Ardhana</p>
+        </div>
+        <div class="float-end">
+            <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
+                by <a href="https://github.com/Suryanataa">Surya</a></p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/components/pagetitle.blade.php
@@ -0,0 +1,3 @@
+<div class="page-heading">
+    <h3>{{ $title }}</h3>
+</div>
