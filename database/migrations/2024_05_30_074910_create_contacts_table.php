78a78c3998e07e6c0ec9405105e977e8a5f567ee
Surya <surya.nata.aardhana@gmail.com>
2024-05-30 18:34:40 +0700

    feat:create contacts model

--- database/migrations/2024_05_30_074910_create_contacts_table.php
+++ database/migrations/2024_05_30_074910_create_contacts_table.php
@@ -11,11 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('email');
+            $table->string('telp');
+            $table->string('linkedin');
+            $table->string('instagram');
+            $table->string('github');
+            $table->string('spotify');
             $table->timestamps();
         });
     }
@@ -25,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contacts');
     }
 };
