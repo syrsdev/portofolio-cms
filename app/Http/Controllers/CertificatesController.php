30795a581ca72096ccdb35ee8b8c36c5282b7d8a
Surya <surya.nata.aardhana@gmail.com>
2024-06-01 22:20:23 +0700

    chore: fix typo certificates

--- app/Http/Controllers/CertificatesController.php
+++ app/Http/Controllers/CertificatesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SertificatesController extends Controller
+class CertificatesController extends Controller
 {
     /**
      * Display a listing of the resource.
--- database/migrations/2024_06_01_151340_create_certificates_table.php
+++ database/migrations/2024_06_01_151340_create_certificates_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sertificates', function (Blueprint $table) {
+        Schema::create('certificates', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sertificates');
+        Schema::dropIfExists('certificates');
     }
 };
