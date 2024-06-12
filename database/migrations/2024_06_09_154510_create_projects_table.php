b7cae622be45ab7178cabc962ac81caa7e6a6d09
Surya <surya.nata.aardhana@gmail.com>
2024-06-12 22:16:05 +0700

    feat: make model experience & projects

---
+++ database/migrations/2024_06_09_154510_create_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->string('description');
+            $table->string('link');
+            $table->string('github_link');
+            $table->string('figma_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
---
+++ database/migrations/2024_06_09_154926_create_experiences_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->id();
+            $table->string('position');
+            $table->string('company');
+            $table->string('location');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->foreignId('status_id')->constrained('work_statuses');          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experiences');
+    }
+};
