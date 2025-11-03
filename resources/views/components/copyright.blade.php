ea1f19d28f5cad225bd2e6c30c20a344986a23c1
Surya Nata Ardhana <surya.nata.aardhana@gmail.com>
2025-11-03 19:56:00 +0700

    chore: update copyright year in footer component

--- resources/views/components/copyright.blade.php
+++ resources/views/components/copyright.blade.php
@@ -5,7 +5,7 @@
         </div>
         <div class="float-end">
             <p>Crafted with <span class="text-danger"><i class="bi bi-heart-fill icon-mid"></i></span>
-                by <a href="https://github.com/Suryanataa">Surya</a></p>
+                by <a href="https://github.com/syrsdev" target="_blink">syrsdev</a></p>
         </div>
     </div>
 </footer>
